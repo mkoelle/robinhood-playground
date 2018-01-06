a63de534c9cf68a7ecca26733157e979996f49d7
John Murphy <chiefsmurph@gmail.com>
2018-01-06 15:13:56 -0800

    login

--- rh-actions/login.js
+++ rh-actions/login.js
@@ -1,9 +1,9 @@
 const { credentials } = require('../config');
 
 module.exports = () => {
-  return new Promise((resolve) => {
-    const Robinhood = require('robinhood')(credentials, () => {
-      resolve(Robinhood);
+    return new Promise((resolve) => {
+        const Robinhood = require('robinhood')(credentials, () => {
+            resolve(Robinhood);
+        });
     });
-  });
 };
