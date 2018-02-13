e71c94b3c311254c850eed9aa2f165c7d98eaa0b
John Murphy <chiefsmurph@gmail.com>
2018-02-12 21:27:48 -0800

    swings

--- modules/swings.js
+++ modules/swings.js
@@ -170,7 +170,7 @@ const swings = {
         // runs at init
         regCronIncAfterSixThirty(Robinhood, {
             name: 'execute swings strategy',
-            run: [190, 250, 895], // 10:41am, 11:31am
+            run: [190, 250], // 10:41am, 11:31am
             // run: [],
             fn: async (Robinhood, min) => {
                 await executeStrategy(Robinhood, trendFilter, min, 0.3, 'swings', DISABLED);
