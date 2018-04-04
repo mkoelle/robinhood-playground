5ac4217b77fbe2e1144e8927667758e3910fc6ae
xTv <chiefsmurph@gmail.com>
2018-04-04 00:28:17 -0700

    disabling all strategies tomorrow

--- strategies-enabled.js
+++ strategies-enabled.js
@@ -1,28 +1,3 @@
 module.exports = [
 
-    // 7 day wrap up
-    // 100% up
-    'ups-then-downs-shiftedHist-tenTo15-10',
-
-    // 80%+
-    'daytime-fiveTo10-260',
-    'daytime-200',
-    'cheapest-picks-chp1-4',
-
-    // 3 day wrap up
-    // 100% up
-    'cheapest-picks-chp3-175',
-    'week-swings-fiveTo10-1',
-    'ups-then-downs-shiftedHist-tenTo15-200',
-
-    // 2 day wrap up
-    // 100% up
-    'constant-risers-10minute-percUpCloseOnly-filtered40-40',
-
-
-
-
-    'constant-risers-5minute-percUpCloseOnlyPoints-filtered50-300',
-    'constant-risers-5minute-percUpCloseOnlyPoints-filtered50-250',
-
 ];
