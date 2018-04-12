2e3bbcc050bf40be691da95c5c79f6b44a2e9525
murphyjo <John.Murphy@ask.com>
2018-04-11 19:51:20 -0700

    ready for 4-12 bring it on!

--- strategies-enabled.js
+++ strategies-enabled.js
@@ -1,17 +1,21 @@
 const hotStrats = [
-    'week-swings-tenTo15-1',
+    'dynamo-3000-top50tso-onlyWatchout-5lowestsharesToCap-351',
     'constant-risers-5minute-percUpHighClosePoints-140',
-    'dynamo-3000-middle50tso-overall-5lowestYP-200',
+    'constant-downers-5minute-percDownLowClosePoints-filtered60-242',
+    'sudden-drops-last30trend-fiveTo10-388',
+    'dynamo-3000-top50tso-onlyWatchout-5lowestsharesToCap-381',
+    'dynamo-3000-bottom50tso-notWatchout-5lowestsharesToCap-tenTo15-40',
+    'constant-risers-10minute-percUpHighClose-250',
+    'based-on-jump-down3overnight-ltneg50percmax-fiveTo10-5',
+    'fizbiz-under5Target10Change2Vol200Within10of52Low-75',
+    'dow-historical-sortedByAvgToday-73',
+    'up-streak-3to5days-ltneg1overnight-fiveTo10-189',
+    'based-on-jump-down3overnight-trending35257-5',
+    'dynamo-3000-middle50tso-onlyWatchout-5lowestYP-4',
+    'based-on-jump-down3overnight-gtneg20percmax-fiveTo10-30',
+    'constant-downers-5minute-percDownCloseOnlyPoints-filtered40-242',
+    'dow-historical-sortedByAvgToday-73',
     'constant-risers-10minute-percUpHighClosePoints-300',
-    'constant-risers-10minute-percUpHighClosePoints-filtered60-100',
-    'constant-risers-5minute-percUpHighClosePoints-300',
-    'based-on-jump-down3overnight-gtneg20percmax-fiveTo10-5',
-    'based-on-jump-up3overnight-ltneg50percmax-tenTo15-5',
-    'constant-risers-10minute-percUpHighClosePoints-filtered60-140',
-    'sudden-drops-last5trend-fiveTo10-328',
-    'sudden-drops-last10trend-388',
-    'based-on-jump-down3overnight-ltneg50percmax-fiveTo10-16',
-    'ups-then-downs-prevClose-fiveTo10-300'
 ];
 
 module.exports = {
