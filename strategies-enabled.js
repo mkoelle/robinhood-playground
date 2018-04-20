64ba686c52d0e47d5004537bb94d20c4184a0863
xTv <chiefsmurph@gmail.com>
2018-04-19 22:46:37 -0700

    manual strats for tomorrow

--- strategies-enabled.js
+++ strategies-enabled.js
@@ -2,35 +2,36 @@ const mySpecialSauce = [
     'based-on-jump-down3overnight-trending35257-5'
 ];
 
-const moreGoodStuff = [
-    'constant-risers-10minute-percUpCloseOnlyPoints-filtered60-250',
-    'sudden-drops-last1trend-328',
-    'dynamo-3000-bottom50tso-notWatchout-5highestsharesToCap-200',
-    'dynamo-3000-middle50tso-volumetoavg-40',
-    'dynamo-3000-top50tso-overall-5lowestYP-40',
-    'constant-downers-10minute-percDownCloseOnlyPoints-filtered40-77',
-    'sudden-drops-last2trend-tenTo15-14',
-    'dynamo-3000-bottom50tso-notWatchout-5lowestsharesToCap-tenTo15-200'
-];
-
 const forPurchase = [
-    '[mySpecialSauce]',
-    '[mySpecialSauce]',
-    '[mySpecialSauce]',
-    '[mySpecialSauce]',
-    '[wild n crazy]',
-
-    'null',
-
-    '[5day-sortedByAvgTrend-first10]',
-    '[4IncTodayCount2-sortedByAvgTrend-uniq-first10]',
-    '[5dayCount2-sortedByAvgTrend-uniq-first10]',
-    '[4IncToday-sortedByAvgTrend-uniq-first5]',
-    '[10IncTodayCount6-sortedByAvgTrend-first10]',
-
-    'null',
-
-    '[moreGoodStuff]'
+    'based-on-jump-down3overnight-trending35257-5',
+    'based-on-jump-down3overnight-trending35257-5',
+    'based-on-jump-down3overnight-trending35257-5',
+    'based-on-jump-down3overnight-trending35257-5',
+    'based-on-jump-down3overnight-trending35257-16',
+    'based-on-jump-down3overnight-trending35257-notWatchout-fiveTo10-16',
+    'based-on-jump-down3overnight-trending35257-notWatchout-fiveTo10-16',
+    'based-on-jump-down3overnight-gtneg20percmax-fiveTo10-5',
+    'based-on-jump-up3overnight-trending35257-fiveTo10-5',
+    'based-on-jump-down3overnight-trending35257-gtneg20percmax-5',
+    'dynamo-3000-bottom50tso-trendingUp3010-5highestYP-fiveTo10-4',
+    'sudden-drops-last10trend-221',
+    'sudden-drops-last10trend-fiveTo10-328',
+    'dynamo-3000-bottom50tso-notWatchout-5lowestTSO-200',
+    'sudden-drops-last2trend-tenTo15-14',
+    'dynamo-3000-top50tso-topPE-351',
+    'up-streak-5days-ltneg3overnight-189',
+    'sudden-drops-last10trend-221',
+    'constant-downers-10minute-percDownLowClosePoints-filtered40-77',
+    'constant-downers-10minute-percDownLowClose-filtered40-77',
+    'constant-downers-10minute-percDownLowClose-filtered40-77',
+    'constant-downers-10minute-percDownLowClose-filtered40-77',
+    'dynamo-3000-bottom50tso-overall-381',
+    'constant-risers-10minute-percUpHighClosePoints-filtered60-300',
+    'ups-then-downs-shiftedHist-tenTo15-380',
+    'dynamo-3000-bottom50tso-onlyWatchout-5lowestsharesToCap-fiveTo10-4',
+    'dynamo-3000-bottom50tso-onlyWatchout-5lowestsharesToCap-fiveTo10-4',
+    'dynamo-3000-bottom50tso-trendingUp3010-5lowestsharesToCap-fiveTo10-4',
+    'dynamo-3000-bottom50tso-trendingUp3010-5lowestsharesToCap-fiveTo10-4'
 ];
 
 module.exports = {
@@ -47,7 +48,6 @@ module.exports = {
             'week-swings-tenTo15-1',
             'constant-risers-10minute-percUpHighClosePoints-300'
         ],
-        moreGoodStuff,
-        mySpecialSauce,
+        mySpecialSauce
     }
 }
