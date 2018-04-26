ac4aac3080f150b6ad4ce1c3c01f1186cad2009e
xTv <chiefsmurph@gmail.com>
2018-04-26 00:35:22 -0700

    <3 this thing

--- socket-server/strat-manager.js
+++ socket-server/strat-manager.js
@@ -34,7 +34,7 @@ const stratManager = {
         await this.sendStrategyReport();
         console.log('initd strat manager');
 
-        new CronJob(`23 6 * * 1-5`, () => this.newDay(), null, true);
+        new CronJob(`21 6 * * 1-5`, () => this.newDay(), null, true);
 
         setInterval(() => this.getRelatedPrices(), 40000);
     },
@@ -77,7 +77,7 @@ const stratManager = {
         const now = new Date();
         const compareDate = new Date();
         compareDate.setHours(6);
-        compareDate.setMinutes(23);
+        compareDate.setMinutes(21);
         if (compareDate - now > 0) {
             now.setDate(now.getDate() - 1);
         }
