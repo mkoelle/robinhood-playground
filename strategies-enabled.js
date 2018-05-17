4526559d4df342de5d369098fc47848495e3cddf
xTv <chiefsmurph@gmail.com>
2018-05-16 21:12:14 -0700

    may 16

--- strategies-enabled.js
+++ strategies-enabled.js
@@ -27,23 +27,28 @@ const longtermhot = [
 ];
 
 const forPurchase = [
-    'low-float-high-volume-floatTimesabsVolPoints-trenddown3to10-150',
-    'low-float-high-volume-floatTimesvolTo2WeekPoints-trend5to10-95',
-    'low-float-high-volume-floatTimesvolTo2WeekPoints-trenddown1to3-210',
-    'low-float-high-volume-floatTimestwoWeekVolToAvgPoints-trenddown3to10-fiveTo10-95',
-    'based-on-jump-oneToFourOvernight-trending607-shouldWatchout-first1-fiveTo10-0',
-    'constant-risers-5minute-percUpHighClosePoints-filtered60-100',
     'based-on-jump-down3overnight-trending607-notWatchout-first1-tenTo15-16',
+    'sudden-drops-last18trend-first2-fiveTo10-388',
+    'based-on-jump-down3overnight-trending607-notWatchout-gtneg20percmax-fiveTo10-0',
+    'low-float-high-volume-floatTimestwoWeekVolToAvgPoints-25',
     'dynamo-3000-bottom50tso-twoweekvolumetoavg-fiveTo10-4',
-    'sudden-drops-last5trend-first2-fiveTo10-328',
-    'dynamo-3000-middle50tso-overall-lowestYP-fiveTo10-4',
-    'constant-downers-10minute-percDownLowClosePoints-filtered40-77',
-    'sudden-drops-last3trend-first2-fiveTo10-328',
+    'low-float-high-volume-floatTimesvolTo2WeekPoints-trend3to5-tenTo15-25',
+    'dynamo-3000-middle50tso-onlyWatchout-lowestYP-fiveTo10-4',
+    'dynamo-3000-bottom50tso-twoweekvolumetoavg-fiveTo10-4',
+    'low-float-high-volume-floatTimestwoWeekVolToAvgPoints-315',
+    'low-float-high-volume-floatTimestwoWeekVolToAvgPoints-trend3to5-210',
+    'sudden-drops-last1trend-first1-32',
+    'low-float-high-volume-floatTimesvolTo2WeekPoints-trenddown1to3-tenTo15-6',
+    'low-float-high-volume-floatTimesvolToAvgPoints-trenddowngt10-25',
+    'based-on-jump-oneToFourOvernight-trending35257-shouldWatchout-first2-fiveTo10-30',
+    'dynamo-3000-overall-notWatchout-highestYP-fiveTo10-4',
+    'based-on-jump-fourToEightOvernight-trending35257-notWatchout-first2-fiveTo10-0',
+    'low-float-high-volume-floatTimesvolTo2WeekPoints-trend5to10-95',
+    'based-on-jump-fourToEightOvernight-notWatchout-first1-fiveTo10-16',
+    'dynamo-3000-overall-notWatchout-highestYP-4',
+    'based-on-jump-oneToFourOvernight-trending35257-shouldWatchout-first1-fiveTo10-0',
+    ''
 
-    '[16Count5-topPerformers85-uniq-first1]',
-    '[16Count5-topPerformers85-uniq-first1]',
-    '[16Count5-topPerformers85-uniq-first1]',
-    '[10IncTodayCount6-topPerformers85-first3]'
 ];
 
 const cheapestPicksEmailObj = [
