5b068f32201bb1c402491df39b67be8a4b348e92
xTv <chiefsmurph@gmail.com>
2018-05-31 01:37:04 -0700

    now is the time to seize the day

--- strategies-enabled.js
+++ strategies-enabled.js
@@ -50,25 +50,36 @@ const myPicks = [
 ];
 
 const forPurchase = [
+    // class a
     'sudden-drops-last10trend-filter20-63',
     'sudden-drops-last10trend-filter20-63',
+    'low-float-high-volume-floatTimesfloatToVolume-trenddown3to10-25',
+    'low-float-high-volume-floatTimesfloatToVolume-trenddown3to10-25',
+    'based-on-jump-down3overnight-trending35257-shouldWatchout-first1-fiveTo10-5',
+    'based-on-jump-down3overnight-trending35257-shouldWatchout-first1-fiveTo10-5',
 
-    'low-float-high-volume-floatTimesabsVolPoints-trendgt10-210',
+    // class b
     'low-float-high-volume-floatTimesvolTo2WeekPoints-trenddown5to7-tenTo15-315',
+    'based-on-jump-down3overnight-trending607-first1-tenTo15-16',
+    'dynamo-3000-top50tso-onlyWatchout-highestYP-tenTo15-4',
+
+    // risky
+    'based-on-jump-fourToEightOvernight-notWatchout-first1-0',
+
+    // old faithful
+    'dynamo-3000-top50tso-onlyWatchout-highestTSO-200',
+    'dynamo-3000-top50tso-onlyWatchout-highestTSO-200',
+
+    // not enough info
+    'low-float-high-volume-floatTimesabsVolPoints-trendgt10-210',
+    'based-on-jump-fourToEightOvernight-trending53-gt500kvolume-first2-fiveTo10-30',
     'low-float-high-volume-floatTimestwoWeekVolToAvgPoints-trend15to25-95',
     'low-float-high-volume-floatTimesabsVolPoints-trend10to15-notWatchout-95',
 
-    'based-on-jump-fourToEightOvernight-notWatchout-first1-0',
-
-    'dynamo-3000-top50tso-onlyWatchout-highestTSO-200',
-    'based-on-jump-down3overnight-trending607-first1-tenTo15-16',
-    'based-on-jump-down3overnight-trending35257-shouldWatchout-first1-fiveTo10-5',
+    // who knows
+    'constant-risers-5minute-percUpHighClosePoints-filtered50-140',
     'based-on-jump-fourToEightOvernight-notWatchout-first1-fiveTo10-0',
-    'based-on-jump-down3overnight-trending607-first3-tenTo15-16',
     'based-on-jump-down3overnight-trending35257-shouldWatchout-first1-fiveTo10-30',
-    'based-on-jump-fourToEightOvernight-trending53-gt500kvolume-first2-fiveTo10-30',
-    'dynamo-3000-top50tso-onlyWatchout-highestYP-tenTo15-4',
-    'constant-risers-5minute-percUpHighClosePoints-filtered50-140'
 
     // short sell / buy put option / sell call option:
     // based-on-jump-gtEightOvernight-trending53-first1-5
