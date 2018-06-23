c1ac25d1765980e8b05051401f242b2a84c2853f
xTv <chiefsmurph@gmail.com>
2018-06-23 16:48:10 -0700

    add minCount perms to my-recs, including 2to4 perm

--- pms/my-recs.js
+++ pms/my-recs.js
@@ -4,19 +4,61 @@ const { uniqifyArrayOfStrategies } = require('../utils/uniqify-stuff');
 module.exports = async (Robinhood) => {
 
     const perms = [
-        [20, 15],
-        [10, 5],    // days, count
-        [7, 4],
-        [5, 3],
-        [3, 2]
+        {
+            dayCount: 1,
+            minCounts: [1, 2]
+        },
+        // {
+        //     dayCount: 3,
+        //     minCounts: [2, 4]
+        // },
+        // {
+        //     dayCount: 5,
+        //     minCounts: [2, 3, 4]
+        // },
+        {
+            dayCount: 7,
+            minCounts: [3, 5]
+        },
+        // {
+        //     dayCount: 10,
+        //     minCounts: [2, 5, 7, 9]
+        // },
+        // {
+        //     dayCount: 15,
+        //     minCounts: [2, 5, 9, 13]
+        // },
+        // {
+        //     dayCount: 20,
+        //     minCounts: [2, 5, 10, 13, 17]
+        // },
+        // {
+        //     dayCount: 25,
+        //     minCounts: [7, 13, 18, 23]
+        // }
     ];
 
     let resultObj = {};
 
-    for (let [day, minCount] of perms) {
-        const { sortedByPercUp } = await stratPerfOverall(Robinhood, true, day, minCount);
-        console.log(sortedByPercUp);
+    const getNames = arr => arr.map(pick => pick.name);
+    const uniqifyAndAddPM = (prefix, picksObj) => {
+        const withUniq = (name, list) => ({
+            [`${prefix}-${name}`]: getNames(list),
+            [`${prefix}-uniq-${name}`]: getNames(uniqifyArrayOfStrategies(list))
+        });
+        resultObj = {
+            ...resultObj,
+            ...Object.keys(picksObj).reduce((acc, val) => ({
+                ...acc,
+                ...withUniq(
+                   val,
+                   picksObj[val]
+                )
+            }), {})
+        };
+    };
 
+    const createPicksObjFromSortedByPercUp = sortedByPercUp => {
         const hundredUpPicks = sortedByPercUp
             .filter(pick => pick.percUp === 1);
 
@@ -29,32 +71,43 @@ module.exports = async (Robinhood) => {
         const hundredWithHundredCheck = hundredUpTrendsAllGt1
             .filter(pick => pick.hundredResult > 135);
 
-        const picks = {
+        const picksObj = {
             hundredUpPicks,
             hundredUpAvgGt4,
             hundredUpTrendsAllGt1,
             hundredWithHundredCheck
         };
 
-        const getNames = arr => arr.map(pick => pick.name);
+        return picksObj;
+    };
 
-        const prefix = `day${day}count${minCount}`;
-        const withUniq = (name, list) => ({
-            [`${prefix}-${name}`]: getNames(list),
-            [`${prefix}-uniq-${name}`]: getNames(uniqifyArrayOfStrategies(list))
-        });
-
-        resultObj = {
-            ...resultObj,
-            ...Object.keys(picks).reduce((acc, val) => ({
-                ...acc,
-                ...withUniq(
-                   val,
-                   picks[val]
-                )
-            }), {})
+    for (let { dayCount, minCounts } of perms) {
+        console.log('here')
+        const { sortedByPercUp } = await stratPerfOverall(Robinhood, true, dayCount);
+        console.log('hi')
+        const filterSortedByPercUpAndAddToResults = (prefix, filterFn) => {
+            const filteredStratPerf = sortedByPercUp.filter(filterFn);
+            console.log('---filtered')
+            const picksObj = createPicksObjFromSortedByPercUp(filteredStratPerf);
+            console.log('---picksobj')
+            uniqifyAndAddPM(prefix, picksObj);
+            console.log('---uniq')
         };
-
+        console.log('defined')
+        minCounts.forEach(minCount => {
+            console.log('bingo');
+            filterSortedByPercUpAndAddToResults(
+                `day${dayCount}count${minCount}`,
+                pick => pick.count >= minCount
+            );
+            console.log('bango')
+        });
+        // create count2to4
+        filterSortedByPercUpAndAddToResults(
+            `day${dayCount}count2to4`,
+            pick => pick.count >= 2 && pick.count <= 4
+        );
+        console.log('done')
     }
 
     return resultObj;
