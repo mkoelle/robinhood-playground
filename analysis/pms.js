2740ea87d53d093b346b6f8005fffc6f43146b72
xTv <chiefsmurph@gmail.com>
2018-07-16 00:37:02 -0700

    last week

--- analysis/pms.js
+++ analysis/pms.js
@@ -49,8 +49,8 @@ module.exports = async (Robinhood, daysBack = 5, minCount = 5) => {
             ...pmAnalysis[pm]
         }))
         .sort((a, b) => b.avgTrend - a.avgTrend)
-        .filter(t => t.percUp > .8)
-        .filter(t => t.trends.length >= minCount)// && t.trends.every(a => a > -1));
+        // .filter(t => t.hundredResult > 110)
+        // .filter(t => t.trends.length >= minCount)// && t.trends.every(a => a > -1));
     console.log(JSON.stringify(sortedArray, null, 2));
 
 
