83e8c1decbddad183db0bae7d92bb55bf1c8e21b
xTv <chiefsmurph@gmail.com>
2018-07-24 14:53:35 -0700

    first implementation of strategy-perf-multiple !!! gold stars

--- analysis/strategy-perf-multiple.js
+++ analysis/strategy-perf-multiple.js
@@ -43,145 +43,115 @@ module.exports = async (Robinhood, includeToday, daysBack = NUM_DAYS, minCount =
         .map(f => f.split('.')[0])
         .sort((a, b) => new Date(a) - new Date(b));
 
-    if (paramTrue(ignoreYesterday)) sortedFiles.pop();
-    let threeMostRecent = sortedFiles.slice(0 - daysBack);
-    console.log('selected days', threeMostRecent);
+    let days = sortedFiles.slice(0 - daysBack);
+    console.log('selected days', days);
 
     const stratResults = new HashTable();
-    for (let day of threeMostRecent) {
-
+    const stratObj = {};
+    for (let day of days) {
         const dayStrats = await jsonMgr.get(`./strat-perfs/${day}.json`);
-        Object.keys(dayStrats).forEach(period => {
-
-            const sellMin = Number(period.substring(period.lastIndexOf('-') + 1));
-            // if (period !== 'next-day-9') return; // only consider 9 minute sell times
-            dayStrats[period].forEach(stratPerf => {
-                if (stratPerf.avgTrend > 100) return;       // rudimentary filter out reverse splits's
-                const split = stratPerf.strategyName.split('-');
-                const buyMin = Number(split.pop());
-                const strategyName = split.join('-');
-                const key = {
-                    strategyName,
-                    buyMin,
-                    sellTime: period
-                };
-                stratResults.put(key, (stratResults.get(key) || []).concat(stratPerf.avgTrend));
-            });
-
-        });
+        stratObj[day] = dayStrats;
     }
 
-    // should includetoday?
-    // if (paramTrue(includeToday) || typeof includeToday === 'object') {
-    //     console.log('adding today');
-    //     const todayPerf = typeof includeToday === 'object' ? includeToday : await strategyPerfToday(Robinhood);
-    //     todayPerf.forEach(perf => {
-    //         let { strategyName, avgTrend } = perf;
-    //         const lastDash = strategyName.lastIndexOf('-');
-    //         const buyMin = Number(strategyName.substring(lastDash + 1));
-    //
-    //
-    //         strategyName = strategyName.substring(0, lastDash);
-    //         const key = {
-    //             strategyName,
-    //             buyMin
-    //         };
-    //
-    //         // const curCount = (stratResults.get(key) || []).length;
-    //         // console.log(curCount, 'curcount');
-    //         // console.log(key, 'key', avgTrend);
-    //
-    //         // const trendCurCountTimes = Array(curCount || 1).fill(avgTrend);
-    //         stratResults.put(key, (stratResults.get(key) || []).concat(avgTrend));
-    //     });
-    //     // console.log('turkey true', todayPerf);
-    // }
+    const allStrategies = stratObj[days[0]]['next-day-9'].map(obj => obj.strategyName);
+    // const allStrategies = [
+    //     'constant-risers-5minute-percUpHighClosePoints-lowovernightjumps-100',
+    //     'based-on-jump-down3overnight-trending35257-notWatchout-first1-fiveTo10-30',
+    //     'low-float-high-volume-volToAvgPoints-trend5to10-6'
+    // ];
 
-    stratResults.keys().forEach(keyObj => {
-        const arrayOfTrends = stratResults.get(keyObj);
-        console.log('heyobj', keyObj, arrayOfTrends);
-        // stratResults.put(keyObj, {
-        //     avgTrend: avgArray(arrayOfTrends),
-        //     count: arrayOfTrends.length,
-        //     trends: arrayOfTrends
-        // });
+    const analyze = (strategy, date) => {
+        const foundTrends = [];
+        const strat = stratObj[date];
+        Object.keys(strat)
+            // .filter(key => key.includes('next-day-9'))
+            .forEach(key => {
+                const foundObj = strat[key].find(obj => obj.strategyName === strategy);
+                foundObj && foundTrends.push({
+                    ...foundObj,
+                    key
+                });
+            });
+
+        // console.log(foundTrends);
+
+        const allKeys = foundTrends.map(obj => obj.key);
+        const requiredKeys = ['next-day-9'];
+
+        if (
+            allKeys.length < 0
+            // !requiredKeys.every(key => allKeys.includes(key))
+        ) {
+            // console.log('has', allKeys);
+            return {
+                notEnoughError: true
+            };
+        }
+
+        if (!foundTrends.length) {
+            return null;
+        }
+
+        const sorted = foundTrends.sort((a, b) => b.avgTrend - a.avgTrend);
+        const trends = foundTrends.map(trend => trend.avgTrend);
+
+        const winnerTime = sorted[0].key;
+        const stats = {
+            didGoUp: trends.some(trend => trend > 0),
+            maxUp: sorted[0].avgTrend,
+            winnerTime,
+            date
+        };
+
+        return stats;
+    };
+
+
+    const allRoundup = [];
+
+    allStrategies.forEach((strategy, index) => {
+
+        // const trendObjs = [];
+
+        const analyzed = days
+            .map(day => analyze(strategy, day))
+            .filter(value => !!value);
+
+        const withErrors = analyzed
+            .filter(value => value.notEnoughError);
+
+        // console.log('num errors', withErrors.length);
+
+        const withoutErrors = analyzed
+            .filter(value => !value.notEnoughError);
+
+        const roundup = {
+            strategy,
+            percUp: withoutErrors.filter(a => a.didGoUp).length / withoutErrors.length,
+            avgMax: avgArray(
+                withoutErrors
+                    .map(a => a.maxUp)
+                    .filter(trend => trend < 100)
+            ),
+            numErrors: withErrors.length,
+            count: withoutErrors.length,
+            // dates: withoutErrors.map(a => a.date)
+        };
+
+        allRoundup.push(roundup);
+        // console.log(strategy, roundup);
+
+
+        // console.log(trendObjs);
+
+        if (index % 50 === 0) {
+            console.log(index, '/', allStrategies.length);
+        }
+
     });
 
-
-
-    // console
-
-
-
-
-
-    // const allPerfs = [];
-    // stratResults.keys().forEach(keyObj => {
-    //     const strategyPerformance = stratResults.get(keyObj);
-    //     allPerfs.push({
-    //         ...keyObj,
-    //         ...strategyPerformance
-    //     });
-    // });
-    // // stratResults.print();
-    //
-    // // console.log('all', JSON.stringify(allPerfs, null, 2));
-    //
-    // const withoutPerms = allPerfs
-    //     .filter(({ strategyName }) => {
-    //         const lastChunk = strategyName.substring(strategyName.lastIndexOf('-') + 1);
-    //         return !['single', 'first3'].includes(lastChunk);
-    //     })
-    //     .filter(perf => perf.count >= minCount && perf.count <= maxCount);
-    //
-    // const withData = withoutPerms.map(({ strategyName, avgTrend, buyMin, trends, count }) => ({
-    //     name: strategyName + '-' + buyMin,
-    //     avgTrend,
-    //     trends: trends.map(t => Math.round(t)),
-    //     percUp: trends.map(t => Math.round(t)).filter(t => t > 0).length / trends.length,
-    //     hundredResult: trends.reduce((acc, val) => {
-    //         return acc * (100 + val) / 100;
-    //     }, 100),
-    //     count
-    // }));
-    //
-    // const sortedByAvgTrend = withData
-    //     .sort((a, b) => b.avgTrend - a.avgTrend)
-    //     .slice(0);
-    //
-    // // console.log(sortedByAvgTrend);
-    // console.log('sorted by avg trend')
-    // console.table(sortedByAvgTrend);
-    //
-    // const sortedByPercUp = withData
-    //     // .filter(t => t.trend.length > 30)
-    //     // .filter(t => t.trends.filter(trend => trend < 0).length < 8)
-    //     .sort((a, b) => {
-    //         return (b.percUp == a.percUp) ? b.avgTrend - a.avgTrend : b.percUp - a.percUp;
-    //     })
-    //     .slice(0);
-    //
-    // console.log('sorted by perc up')
-    // console.table(sortedByPercUp);
-    //
-    // const sortedByHundredResult = withData
-    //     // .filter(t => t.trend.length > 30)
-    //     // .filter(t => t.percUp === 1)
-    //     // .filter(t => t.name.includes('low-float'))
-    //     .sort((a, b) => {
-    //         return (b.hundredResult == a.hundredResult) ? b.avgTrend - a.avgTrend : b.hundredResult - a.hundredResult;
-    //     })
-    //     .slice(0);
-    //
-    // console.log('sorted by hundred result')
-    // console.table(sortedByHundredResult);
-    //
-    //
-    //
-    // return {
-    //     sortedByAvgTrend,
-    //     sortedByPercUp,
-    //     sortedByHundredResult
-    // };
+    return allRoundup
+        .filter(r => r.count > 10)
+        .sort((a, b) => b.percUp - a.percUp);
 
 };
