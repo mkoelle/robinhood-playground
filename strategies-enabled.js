98c4ee910110797f445eee5b55a76339547c2163
xTv <chiefsmurph@gmail.com>
2018-07-28 11:39:33 -0700

    friday

--- strategies-enabled.js
+++ strategies-enabled.js
@@ -112,7 +112,6 @@ const forPurchase = [
     // short sell / buy put option / sell call option:
     // low-float-high-volume-volTo2WeekPoints-shouldWatchout-25
     // constant-risers-10minute-percUpHighClose-highovernightjumps-198
-    'based-on-jump-down5overnight-notWatchout-gtneg20percmax-first1-fiveTo10-0',
     'low-float-high-volume-floatTimesfloatToVolume-trenddown5to7-fiveTo10-276'
 ];
 
