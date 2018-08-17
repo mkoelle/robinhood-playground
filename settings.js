085c44e7c5ff9501e76cc2f3d5f5fe29887d4ea8
xTv <chiefsmurph@gmail.com>
2018-08-17 06:15:00 -0700

    disabled cheapest picks email for today (already sent)

--- settings.js
+++ settings.js
@@ -31,6 +31,6 @@ module.exports = {
         [['chief', 'sm', 'urph', '@', 'gm', 'ail', '.com'].join('')]: [
             // ...allShorts
         ],
-        ...cheapestPicksEmailObj
+        // ...cheapestPicksEmailObj
     }
 };
