cb2d584556dbeeae3630918d933fe76e4c8f9e1f
John Murphy <chiefsmurph@gmail.com>
2018-09-16 22:57:22 -0700

    spread module ... needs work

---
+++ modules/spread.js
@@ -0,0 +1,75 @@
+// utils
+const regCronIncAfterSixThirty = require('../utils/reg-cron-after-630');
+
+// app-actions
+const executeStrategy = require('../app-actions/execute-strategy');
+const addTrendSinceOpen = require('../app-actions/add-trend-since-open');
+
+Array.prototype.ticks = function() {
+    return this.map(t => t.ticker);
+};
+
+const trendFilter = async (Robinhood, trend) => {
+
+    console.log('total trend stocks', trend.length);
+
+    let withTrendSinceOpen = await addTrendSinceOpen(Robinhood, trend);
+    withTrendSinceOpen = withTrendSinceOpen
+        .map(t => ({
+            ...t,
+            askPrice: Number(t.quote_data.ask_price),
+            bidPrice: Number(t.quote_data.bid_price),
+            lastTrade: Number(t.quote_data.last_trade_price)
+        }))
+        .filter(buy => buy.trendSinceOpen && buy.askPrice && buy.bidPrice && buy.lastTrade)
+        .map(t => ({
+            ...t,
+            spreadAbs: t.askPrice - t.bidPrice
+        }))
+        .map(t => ({
+            ...t,
+            spreadPerc: Math.round(t.spreadAbs / t.lastTrade * 100
+        }));
+    console.log(JSON.stringify(withTrendSinceOpen, null, 2));
+
+    const singleHighestSpreadWithFilter = (filter = () => true) => {
+        return withTrendSinceOpen
+            .filter(filter)
+            .sort((a, b) => b.spreadAbs - a.spreadAbs)
+            .slice(0, 1)
+            .ticks();
+    };
+
+    const bidAskLastTradeSame = (({ quote_data: { ask_price, bid_price, last_trade_price }}) => 
+        ask_price === bid_price && bid_price === last_trade_price
+    );
+    return {
+        bidAskLastTradeSame: withTrendSinceOpen
+            .filter(bidAskLastTradeSame).ticks(),
+        bidAskLastTradeSame500kVolume: withTrendSinceOpen
+            .filter(t => Number(t.fundamentals.volume) > 500000)
+            .filter(bidAskLastTradeSame)
+            .ticks(),
+        singleLargestSpread: singleHighestSpreadWithFilter(),
+        singleLargestSpread100kVolume: singleHighestSpreadWithFilter(t => Number(t.fundamentals.volume) > 100000),
+        singleLargestSpread500kVolume: singleHighestSpreadWithFilter(t => Number(t.fundamentals.volume) > 500000),
+        singleLargestSpread1milVolume: singleHighestSpreadWithFilter(t => Number(t.fundamentals.volume) > 1000000),
+    };
+};
+
+const spread = {
+    trendFilter,
+    init: Robinhood => {
+        // runs at init
+        regCronIncAfterSixThirty(Robinhood, {
+            name: 'execute spread strategy',
+            run: [4, 40, 100, 200, 260, 351, 381], // 10:41am, 11:31am
+            // run: [],
+            fn: async (Robinhood, min) => {
+                await executeStrategy(Robinhood, trendFilter, min, 0.3, 'spread');
+            }
+        });
+    }
+};
+
+module.exports = spread;
