0e5f3ecf5049f46db24356576d9a889079b031e3
John Murphy <chiefsmurph@gmail.com>
2018-09-18 23:40:15 -0700

    calculate avg buy price by looking back at robinhood transactions at weighting the avg based on # of shares

--- analysis/reports/sells.js
+++ analysis/reports/sells.js
@@ -3,6 +3,7 @@
 
 const mapLimit = require('promise-map-limit');
 const jsonMgr = require('../../utils/json-mgr');
+const { avgArray } = require('../../utils/array-math');
 
 const getAssociatedStrategies = require('../../app-actions/get-associated-strategies');
 
@@ -57,14 +58,25 @@ const convertDateToRhFormat = date => {
     return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`;
 };
 
+const calcWeightAvg = transactions => {
+    const totalShares = transactions
+        .map(t => t.quantity)
+        .reduce((acc, val) => acc + val, 0);
+    const numerator = transactions
+        .reduce((acc, t) => {
+            return acc + (t.avgPrice * t.quantity);
+        }, 0)
+    return numerator / totalShares;
+};
+
 module.exports = async (Robinhood, daysBack = 5) => {
 
     // console.log({ mostRecentOnly });
     const dailyTransactionDates = await getFilesSortedByDate('daily-transactions');
-    console.log({ dailyTransactionDates })
+    // console.log({ dailyTransactionDates })
     const robinhoodTransactions = await loadAllRobinhoodTransactions(
         Robinhood, 
-        convertDateToRhFormat(dailyTransactionDates[daysBack])
+        convertDateToRhFormat(dailyTransactionDates[Number(daysBack) + 3])
     );
 
     const analyzeDay = async date => {
@@ -73,26 +85,33 @@ module.exports = async (Robinhood, daysBack = 5) => {
         const fullPath = `./json/daily-transactions/${date}.json`;
         const transactions = await jsonMgr.get(fullPath) || [];
         const dailyTransactionSells = transactions.filter(t => t.type === 'sell');
-
+        // console.log({ dailyTransactionSells});
         // next find sells in Robinhood orders
         const rhDate = convertDateToRhFormat(date);
         // console.log({ rhDate });
-        const rhSells = robinhoodTransactions
+        const formatRhTransaction = order => ({
+            ticker: order.instrument.symbol,
+            avgPrice: Number(order.average_price),
+            quantity: Number(order.quantity),
+            side: order.side
+        });
+        const rhBuys = robinhoodTransactions
+            .filter(t => t.side === 'buy')
+            .map(formatRhTransaction);
+        // console.log({ rhBuys})
+        const rhSellsToday = robinhoodTransactions
             .filter(t => t.updated_at.includes(rhDate))
-            .filter(t => t.side === 'sell');
-        const formattedRobinhoodSells = rhSells.map(sellOrder => ({
-            ticker: sellOrder.instrument.symbol,
-            bid_price: Number(sellOrder.average_price),
-            quantity: sellOrder.quantity,
-        }));
-        // console.log(JSON.stringify({rhSells, date}, null, 2));
+            .filter(t => t.side === 'sell')
+            .map(formatRhTransaction);
+
+        // console.log(JSON.stringify({rhSellsToday, date}, null, 2));
         
-        // console.log({fullPath, dailyTransactionDates, rhSells})
+        // console.log({fullPath, dailyTransactionDates, rhSellsToday})
 
         // combine daily-transaction sells + Robinhood orders sell
         // find and merge with associated buys
         const dailyTransactionTickers = dailyTransactionSells.map(t => t.ticker);
-        const robinhoodSellsNotIncluded = formattedRobinhoodSells
+        const robinhoodSellsNotIncluded = rhSellsToday
             .filter(sell => !dailyTransactionTickers.includes(sell.ticker));
         // console.log({ robinhoodSellsNotIncluded })
         const allSellsToday = [
@@ -108,16 +127,19 @@ module.exports = async (Robinhood, daysBack = 5) => {
         }, dailyTransactionDates);
 
         const combined = allSellsToday.map(sell => {
-            const relatedBuy = associatedBuys.find(obj => obj.ticker === sell.ticker);
+            const matchTicker = obj => obj.ticker === sell.ticker;
+            const relatedDTBuy = associatedBuys.find(matchTicker);
+            const relatedRHBuys = rhBuys.filter(matchTicker);
+            const avgBuyPrice = calcWeightAvg(relatedRHBuys) || relatedDTBuy.bid_price;
             return {
                 ticker: sell.ticker,
-                ...(relatedBuy && {
-                    strategy: relatedBuy.strategy,
-                    buyPrice: relatedBuy.bid_price,
-                    buyDate: relatedBuy.date,
+                ...(relatedDTBuy && {
+                    strategy: relatedDTBuy.strategy,
+                    buyDate: relatedDTBuy.date,
                 }),
+                buyPrice: avgBuyPrice,
                 sellDate: sell.day,
-                sellPrice: sell.bid_price,
+                sellPrice: sell.bid_price || sell.avgPrice,
                 quantity: Number(sell.quantity)
             };
         });
