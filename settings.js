bff7a7a9efee41ff605449d3fe42632a649fb999
John Murphy <chiefsmurph@gmail.com>
2018-09-24 23:14:43 -0700

    throwing darts at a board

--- settings.js
+++ settings.js
@@ -20,7 +20,7 @@ const {
 module.exports = {
     // important settings
     sellAllStocksOnNthDay: 2,
-    purchaseAmt: 70,
+    purchaseAmt: 50,
     forPurchase: [
         '[sepPerfectosLowCounts]',
         '[sepPerfectosLowCounts]',
@@ -41,6 +41,10 @@ module.exports = {
         '[sep17MORE]',
         '[sep17MORE]',
 
+        'sudden-drops-last5trend-filter10-100',
+        'sudden-drops-last5trend-filter10-100',
+        'sudden-drops-last5trend-filter10-100'
+
         '[afterhoursdropsGAMBLES]',
 
         // '[tripleDown]',
@@ -53,10 +57,17 @@ module.exports = {
         // '[hadAGoodDay]', // 3 everydayers
 
         'constant-risers-10minute-percUpCloseOnlyPoints-filtered80-fiveTo10-40',
-        'constant-risers-10minute-percUpCloseOnlyPoints-filtered80-fiveTo10-40',
         'sudden-drops-last3trend-filter10-100',
         'constant-risers-10minute-percUpHighClosePoints-filtered70-80',
-        'constant-risers-10minute-percUpHighClosePoints-filtered80-fiveTo10-80'
+        'low-float-high-volume-floatTimesfloatToVolume-trenddown7to10-25',
+        'low-float-high-volume-absVolPoints-trenddown3to10-shouldWatchout-6',
+        'sudden-drops-last2trend-first1-400',
+        'sudden-drops-last2trend-first1--30',
+        'sudden-drops-last18trend-first2-fiveTo10--10',
+        'first-greens-count1--15',
+        'sudden-drops-last10trend-first2-fiveTo10--50',
+        'sudden-drops-last18trend-first1--50',
+        'sudden-drops-last1trend-first1-tenTo15--10'
     ],
     fallbackSellStrategy: 'limit3',
     force: {
