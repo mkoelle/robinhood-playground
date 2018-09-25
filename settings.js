0277b4a94baf1b90138483f6548f48611e5ade51
John Murphy <chiefsmurph@gmail.com>
2018-09-25 00:19:55 -0700

    working bert

--- settings.js
+++ settings.js
@@ -43,7 +43,7 @@ module.exports = {
 
         'sudden-drops-last5trend-filter10-100',
         'sudden-drops-last5trend-filter10-100',
-        'sudden-drops-last5trend-filter10-100'
+        'sudden-drops-last5trend-filter10-100',
 
         '[afterhoursdropsGAMBLES]',
 
@@ -59,8 +59,10 @@ module.exports = {
         'constant-risers-10minute-percUpCloseOnlyPoints-filtered80-fiveTo10-40',
         'sudden-drops-last3trend-filter10-100',
         'constant-risers-10minute-percUpHighClosePoints-filtered70-80',
+
         'low-float-high-volume-floatTimesfloatToVolume-trenddown7to10-25',
-        'low-float-high-volume-absVolPoints-trenddown3to10-shouldWatchout-6',
+        'low-float-high-volume-floatTimesfloatToVolume-trenddown7to10-25',
+
         'sudden-drops-last2trend-first1-400',
         'sudden-drops-last2trend-first1--30',
         'sudden-drops-last18trend-first2-fiveTo10--10',
